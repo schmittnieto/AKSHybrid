064d46996aefb76a22e549b8dcc5355813ce5047
Cristian Schmitt Nieto <69589673+schmittnieto@users.noreply.github.com>
2024-01-25 21:53:19 +0100

    Update NewAKSWorkerCluster.ps1

--- Script/22H2/NewAKSWorkerCluster.ps1
+++ Script/22H2/NewAKSWorkerCluster.ps1
@@ -25,6 +25,12 @@ function Get-Option ($cmd, $filterproperty) {
   }
 #endregion
 #region Snippet 2: Create New Cluster Network and New AKS Cluster
-
+##Network Deployment 
 #https://learn.microsoft.com/en-us/azure/aks/hybrid/reference/ps/new-akshciclusternetwork
+#$vnet = New-AksHciClusterNetwork -name <String> -vswitchName <String> -gateway <String> -dnsServers <String[]> -ipAddressPrefix <String> -vipPoolStart <IP address> -vipPoolEnd <IP address> -k8sNodeIpPoolStart <IP address> -k8sNodeIpPoolEnd <IP address> -vlanID <int>
+
+##AKS Worker Cluster Deployment
+#https://learn.microsoft.com/de-de/azure/aks/hybrid/reference/ps/new-akshcicluster
+#New-AksHciCluster -name mycluster -controlPlaneVmSize Standard_D4s_v3 -loadBalancerVmSize Standard_A4_v2 -nodePoolName nodepool1 -nodeCount 3 -nodeVmSize Standard_D8s_v3
+
 #endregion
