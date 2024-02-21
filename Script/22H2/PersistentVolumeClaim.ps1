7d608f2c8b6be8d520b1cd69a9cb4ff8153b02f5
Cristian Schmitt Nieto <69589673+schmittnieto@users.noreply.github.com>
2024-02-21 11:45:21 +0100

    Update PersistentVolumeClaim

---
+++ Script/22H2/PersistentVolumeClaim.ps1
@@ -0,0 +1,48 @@
+#region Snippet 1: Functions Get-Option helper function to provide menu
+function Get-Option ($cmd, $filterproperty) {
+    $items = @("")
+    $selection = $null
+    $filteredItems = @()
+    Invoke-Expression -Command $cmd | Sort-Object $filterproperty | ForEach-Object -Begin { $i = 0 } -Process {
+      $items += "{0}. {1}" -f $i, $_.$filterproperty
+      $i++
+    } 
+    $filteredItems += $items | Where-Object { -not [string]::IsNullOrWhiteSpace($_) }
+    $filteredItems | Format-Wide { $_ } -Column 4 -Force | Out-Host
+    #$filteredItems.Count
+    #$filteredItems | Out-Host
+    do {
+      $r = [int]::Parse((Read-Host "Select by number"))
+      $selection = $filteredItems[$r] -split "\.\s" | Select-Object -Last 1
+      if ([String]::IsNullOrWhiteSpace($selection)) { Write-Host "You must make a valid selection" -ForegroundColor Red }
+      else {
+        Write-Host "Selecting $($filteredItems[$r])" -ForegroundColor Green
+      }
+    }until (!([String]::IsNullOrWhiteSpace($selection)))
+      
+    return $selection
+  }
+  #endregion
+  #region Snippet 2: Create Service Barier Token
+  $AKSCluster = Get-Option "Get-AksHciCluster" "Name"
+  $PerStorageName = Read-Host -Prompt 'Input the persistent storage name'
+  $PerStorageCapacity = Read-Host -Prompt 'Input the persistent storage Capacity (Gi) in Numbers: f.E. "10"'
+  $Yaml = "$AKSCluster$PerStorageName.yaml"
+  Get-AksHciCredential -Name $AKSCluster
+  New-Item $Yaml
+  "apiVersion: v1
+  kind: PersistentVolumeClaim
+  metadata:
+   name: $PerStorageName
+  spec:
+   accessModes:
+   - ReadWriteOnce
+   resources:
+    requests:
+     storage: $PerStorageCapacity`Gi " | Out-File $Yaml
+
+  kubectl create -f $Yaml
+
+  #endregion
+  
+  
