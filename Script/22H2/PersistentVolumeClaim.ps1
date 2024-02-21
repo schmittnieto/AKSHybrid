a67fa8b0a1d009e15a37fdac2247d908b6e11728
Cristian Schmitt Nieto <69589673+schmittnieto@users.noreply.github.com>
2024-02-21 11:47:15 +0100

    Update

--- Script/22H2/PersistentVolumeClaim.ps1
+++ Script/22H2/PersistentVolumeClaim.ps1
@@ -1,27 +1,28 @@
 #region Snippet 1: Functions Get-Option helper function to provide menu
 function Get-Option ($cmd, $filterproperty) {
-    $items = @("")
-    $selection = $null
-    $filteredItems = @()
-    Invoke-Expression -Command $cmd | Sort-Object $filterproperty | ForEach-Object -Begin { $i = 0 } -Process {
-      $items += "{0}. {1}" -f $i, $_.$filterproperty
-      $i++
-    } 
-    $filteredItems += $items | Where-Object { -not [string]::IsNullOrWhiteSpace($_) }
-    $filteredItems | Format-Wide { $_ } -Column 4 -Force | Out-Host
-    #$filteredItems.Count
-    #$filteredItems | Out-Host
-    do {
-      $r = [int]::Parse((Read-Host "Select by number"))
-      $selection = $filteredItems[$r] -split "\.\s" | Select-Object -Last 1
-      if ([String]::IsNullOrWhiteSpace($selection)) { Write-Host "You must make a valid selection" -ForegroundColor Red }
-      else {
-        Write-Host "Selecting $($filteredItems[$r])" -ForegroundColor Green
-      }
-    }until (!([String]::IsNullOrWhiteSpace($selection)))
-      
-    return $selection
-  }
+  $items = @("")
+  $selection = $null
+  $filteredItems = @()
+  $i = 0
+  Invoke-Expression -Command $cmd | Sort-Object $filterproperty | ForEach-Object -Process {
+    $items += "{0}. {1}" -f $i, $_.$filterproperty
+    $i++
+  } 
+  $filteredItems += $items | Where-Object { -not [string]::IsNullOrWhiteSpace($_) }
+  $filteredItems | Format-Wide { $_ } -Column 4 -Force | Out-Host
+  #$filteredItems.Count
+  #$filteredItems | Out-Host
+  do {
+    $r = [int]::Parse((Read-Host "Select by number"))
+    $selection = $filteredItems[$r] -split "\.\s" | Select-Object -Last 1
+    if ([String]::IsNullOrWhiteSpace($selection)) { Write-Host "You must make a valid selection" -ForegroundColor Red }
+    else {
+      Write-Host "Selecting $($filteredItems[$r])" -ForegroundColor Green
+    }
+  }until (!([String]::IsNullOrWhiteSpace($selection)))
+    
+  return $selection
+}
   #endregion
   #region Snippet 2: Create Service Barier Token
   $AKSCluster = Get-Option "Get-AksHciCluster" "Name"
